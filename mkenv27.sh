c37e77766acbe65a2fa710808390a51b46099ce7
sven <sven@so36.net>
2012-10-01 12:03:58 +0200

    add: dirty wrapper script v1

---
+++ mkenv27.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Copyleft (C) 2011 by svx and maartentp
+# sven@so36.net, maartentp@milieudefensie.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation , Version 3
+# https://www.gnu.org/copyleft/gpl.html
+#
+
+#dirty wrapper around vritualenv, needs to rewritten
+#just a proof of concept, atm
+
+
+PYTHON_PATH="/home/$USER/opt/python/bin/"
+
+error_exit() {
+        echo "$1"
+        exit 1
+}
+
+case "$1" in
+
+        -h | --help | ? )
+
+                echo "Usage: $0 #NEWENVNAME"
+                exit 1
+        ;;
+
+        * )
+
+                [ -z "$1" ] && error_exit "Need target directory"
+                [ -d "$1" ] && error_exit "Directory $1 already exists"
+
+                eval "$PYTHON_PATH"/virtualenv-2.7 --no-site-packages "$1" || \
+              
+                    error_exit "something went wrong, please investigate"
+        ;;
+esac
+
